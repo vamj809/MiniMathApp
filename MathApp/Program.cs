aaeb687f8e72d1238fd39c83605101002ce7a32c
Víctor A. Montero <vamj809@users.noreply.github.com>
2020-07-16 21:35:08 -0400

    Added ConsoleApp for manual testing

--- MathApp/Program.cs
+++ MathApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace MathApp
 {
@@ -6,7 +7,27 @@ namespace MathApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("--- Hello World! I'm a Mini-Calculator! ---");
+            Console.WriteLine("-------------------------------------------\n");
+
+            Console.WriteLine("Write two integers:");
+            Console.Write("- A:");
+            if(!int.TryParse(Console.ReadLine(), out int a)) {
+                Console.WriteLine("That's not an integer. Try again.");
+                Console.ReadKey();
+                Console.Clear();
+                Main(null);
+                return;
+            }
+            Console.Write("- B:");
+            if (!int.TryParse(Console.ReadLine(), out int b)) {
+                Console.WriteLine("That's not an integer. Try again.");
+                Console.ReadKey();
+                Console.Clear();
+                Main(null);
+                return;
+            }
+            Console.WriteLine($"\n- Result: {MiniCalc.Multiply(a,b)}");
         }
     }
 }
